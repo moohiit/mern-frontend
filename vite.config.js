636e0145169a38292cffbad09dc17f875b369a4b
Mohit Patel <pmohit645@gmail.com>
2024-11-07 17:05:52 +0530

    deployement to ec2

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // Your backend server
+        target: process.env.BASE_URL, // Your backend server
         changeOrigin: true,
         secure: false,
       },
